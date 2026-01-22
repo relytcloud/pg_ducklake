ef101cdfdd3923b08c259f0b5555c8037ac2e5c2
qsliu <57934883+qsliu2017@users.noreply.github.com>
2026-01-22 14:16:56 +0800

    add GUC duckdb.parquet_metadata_cache (#19)
    source-commit: 561118c654e4c364fbe561c13247ddcd514d6550

--- src/pgduckdb_duckdb.cpp
+++ src/pgduckdb_duckdb.cpp
@@ -206,6 +206,8 @@ DuckDBManager::Initialize() {
 	pgduckdb::DuckDBQueryOrThrow(context, "SET TimeZone =" + duckdb::KeywordHelper::WriteQuoted(pg_time_zone));
 	pgduckdb::DuckDBQueryOrThrow(context, "SET default_collation =" +
 	                                          duckdb::KeywordHelper::WriteQuoted(duckdb_default_collation));
+	duckdb::string set_parquet_metadata_cache = duckdb_parquet_metadata_cache ? "true" : "false";
+	pgduckdb::DuckDBQueryOrThrow(context, "SET parquet_metadata_cache =" + set_parquet_metadata_cache);
 	pgduckdb::DuckDBQueryOrThrow(context, "ATTACH DATABASE 'pgduckdb' (TYPE pgduckdb)");
 	pgduckdb::DuckDBQueryOrThrow(context, "ATTACH DATABASE ':memory:' AS pg_temp;");
 
