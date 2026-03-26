6b88e023fd1716f16ae01831c06cfaabf942f58d
qsliu <qsliu2017@outlook.com>
2026-03-26 10:47:41 +0800

    Squashed 'third_party/pg_duckdb/' content from commit f2de4f1b
    git-subtree-dir: third_party/pg_duckdb
    git-subtree-split: f2de4f1b86b8ba3999eb7bb9054f354114656ac3

---
+++ third_party/pg_duckdb_extensions.cmake
@@ -0,0 +1,10 @@
+duckdb_extension_load(json)
+duckdb_extension_load(icu)
+duckdb_extension_load(httpfs
+    GIT_URL https://github.com/duckdb/duckdb-httpfs
+    GIT_TAG 74f954001f3a740c909181b02259de6c7b942632
+)
+duckdb_extension_load(postgres_scanner
+    GIT_URL https://github.com/duckdb/duckdb-postgres
+    GIT_TAG c0e9256a60371a062e8d6cfc4045c71a317b5ee8
+)
